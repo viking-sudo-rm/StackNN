0d71e5ae48b860bb03d73da7333497ab58e0555b
William Merrill <vikingarnir.will@gmail.com>
2017-11-21 11:26:07 -0500

    added batching to the stacks (still  buggy)

--- main.py
+++ main.py
@@ -1,50 +1,222 @@
+# coding: utf-8
+import argparse
+import time
+import math
 import torch
-import torch.autograd as autograd
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+from torch.autograd import Variable
 
-from stack import Stack
+import data
+import model
 
-torch.manual_seed(1)
+parser = argparse.ArgumentParser(description='PyTorch PennTreeBank RNN/LSTM Language Model')
+parser.add_argument('--data', type=str, default='./data/penn',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',
+                    help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
+parser.add_argument('--emsize', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--nhid', type=int, default=200,
+                    help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+parser.add_argument('--lr', type=float, default=20,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=40,
+                    help='upper epoch limit')
+parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                    help='batch size')
+parser.add_argument('--bptt', type=int, default=35,
+                    help='sequence length')
+parser.add_argument('--dropout', type=float, default=0.2,
+                    help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',
+                    help='tie the word embedding and softmax weights')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save', type=str,  default='model.pt',
+                    help='path to save the final model')
+args = parser.parse_args()
 
-class FFController(nn.Module):
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    else:
+        torch.cuda.manual_seed(args.seed)
 
-	def __init__(self, input_size, read_size, output_size):
-		super(FFController, self).__init__()
+###############################################################################
+# Load data
+###############################################################################
 
-		# self.input_size = input_size
-		self.read_size = read_size
-		# self.output_size = output_size
+corpus = data.Corpus(args.data)
 
-		# initialize relevant fields for the model
-		self.linear = nn.Linear(input_size + read_size, 2 + read_size + output_size)
-		self.stack = Stack(read_size)
-		self.read = torch.zeros([1, read_size])
+# Starting from sequential data, batchify arranges the dataset into columns.
+# For instance, with the alphabet as the sequence and batch size 4, we'd get
+# ┌ a g m s ┐
+# │ b h n t │
+# │ c i o u │
+# │ d j p v │
+# │ e k q w │
+# └ f l r x ┘.
+# These columns are treated as independent by the model, which means that the
+# dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+# batch processing.
 
-	def forward(self, x):
-		# print x.shape, self.read.shape
-		output = self.linear(torch.cat([x, self.read], 1))
-		read_params = F.sigmoid(output[:1,:2 + self.read_size])
-		u, d, v = read_params[0,0], read_params[0,1], read_params[:1,2:]
-		self.read = self.stack.forward(v.data, u.data[0], d.data[0])
-		return F.log_softmax(output[0,2 + self.read_size:])
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    if args.cuda:
+        data = data.cuda()
+    return data
 
-model = FFController(3, 10, 2)
-pred = model(autograd.Variable(torch.FloatTensor([[1, 2, 3]]), requires_grad=True))
-print pred
+eval_batch_size = 10
+train_data = batchify(corpus.train, args.batch_size)
+val_data = batchify(corpus.valid, eval_batch_size)
+test_data = batchify(corpus.test, eval_batch_size)
 
-# # the model knows its parameters.  The first output below is A, the second is b.
-# # Whenever you assign a component to a class variable in the __init__ function
-# # of a module, which was done with the line
-# # self.linear = nn.Linear(...)
-# # Then through some Python magic from the Pytorch devs, your module
-# # (in this case, BoWClassifier) will store knowledge of the nn.Linear's parameters
-# for param in model.parameters():
-# 	print(param)
+###############################################################################
+# Build the model
+###############################################################################
 
-# # To run the model, pass in a BoW vector, but wrapped in an autograd.Variable
-# sample = data[0]
-# bow_vector = make_bow_vector(sample[0], word_to_ix)
-# log_probs = model(autograd.Variable(bow_vector))
-# print(log_probs)
+ntokens = len(corpus.dictionary)
+model = model.FFController(ntokens, 50, 50, ntokens)
+if args.cuda:
+    model.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+###############################################################################
+# Training code
+###############################################################################
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+# get_batch subdivides the source data into chunks of length args.bptt.
+# If source is equal to the example output of the batchify function, with
+# a bptt-limit of 2, we'd get the following two Variables for i = 0:
+# ┌ a g m s ┐ ┌ b h n t ┐
+# └ b h n t ┘ └ c i o u ┘
+# Note that despite the name of the function, the subdivison of data is not
+# done along the batch dimension (i.e. dimension 1), since that was handled
+# by the batchify function. The chunks are along dimension 0, corresponding
+# to the seq_len dimension in the LSTM.
+
+def get_batch(source, i, evaluation=False):
+    seq_len = min(args.bptt, len(source) - 1 - i)
+    data = Variable(source[i:i+seq_len], volatile=evaluation)
+    target = Variable(source[i+1:i+1+seq_len].view(-1))
+    return data, target
+
+
+def evaluate(data_source):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_stack()
+    for i in range(0, data_source.size(0) - 1, args.bptt):
+        data, targets = get_batch(data_source, i, evaluation=True)
+        output = model(data)
+        output_flat = output.view(-1, ntokens)
+        total_loss += len(data) * criterion(output_flat, targets).data
+        hidden = repackage_hidden(hidden)
+    return total_loss[0] / len(data_source)
+
+def train():
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+        data, targets = get_batch(train_data, i)
+        model.zero_grad()
+        # FIXME right now looping naively without batching
+        loss = 0.
+        for i in xrange(data.data.shape[1]):
+            v = data[:,i]
+            print "size", len(v)
+            model.init_stack()
+            # for c in v:
+            #     model(c)
+            # TODO the prediction is the concatenation of all the vectors (35L, 20L) -> (700L,)
+            pred = torch.cat([model(c) for c in v], 0)
+            # pred = torch.cat(pred, 0)
+            print targets.data.shape, pred.data.shape
+            # pred = torch.FloatTensor()
+            # print pred
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+        for p in model.parameters():
+            p.data.add_(-lr, p.grad.data)
+
+        total_loss += loss.data
+
+        if batch % args.log_interval == 0 and batch > 0:
+            cur_loss = total_loss[0] / args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                epoch, batch, len(train_data) // args.bptt, lr,
+                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+# Loop over epochs.
+lr = args.lr
+best_val_loss = None
+
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    for epoch in range(1, args.epochs+1):
+        epoch_start_time = time.time()
+        train()
+        val_loss = evaluate(val_data)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                           val_loss, math.exp(val_loss)))
+        print('-' * 89)
+        # Save the model if the validation loss is the best we've seen so far.
+        if not best_val_loss or val_loss < best_val_loss:
+            with open(args.save, 'wb') as f:
+                torch.save(model, f)
+            best_val_loss = val_loss
+        else:
+            # Anneal the learning rate if no improvement has been seen in the validation dataset.
+            lr /= 4.0
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
+
+# Load the best saved model.
+with open(args.save, 'rb') as f:
+    model = torch.load(f)
+
+# Run on test data.
+test_loss = evaluate(test_data)
+print('=' * 89)
+print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+    test_loss, math.exp(test_loss)))
+print('=' * 89)
--- stack.py
+++ stack.py
@@ -6,45 +6,50 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
+def relu(t):
+	z = torch.zeros(t.shape)
+	return torch.max(z, t)
+
 class Stack(nn.Module):
 
-	def __init__(self, embedding_size):
+	def __init__(self, batch_size, embedding_size):
 		super(Stack, self).__init__()
 
-		# initialize the datastructure
-		self.V = torch.FloatTensor(0, embedding_size)
+		# initialize tensors
+		self.V = torch.FloatTensor(0)
 		self.s = torch.FloatTensor(0)
 
-		# an instance of the ReLU function
-		# self.relu = nn.ReLU()
-		self.relu = lambda x: max(0, x)
+		self.batch_size = batch_size
+		self.embedding_size = embedding_size
 
 	def forward(self, v, u, d):
 		"""
-		@param v vector of dimension [1, embedding_size] to push
-		@param u pop signal in (0, 1)
-		@param d push signal in (0, 1)
-		@return read vector of dimension [1, embedding_size]
+		@param v [batch_size, embedding_size] matrix to push
+		@param u [batch_size,] vector of pop signals in (0, 1)
+		@param d [batch_size,] vector of push signals in (0, 1)
+		@return [batch_size, embedding_size] read matrix
 		"""
 
-		# update V
+		# update V, which is of size [t, bach_size, embedding_size]
+		v = v.view(1, self.batch_size, self.embedding_size)
 		self.V = torch.cat([self.V, v], 0)
 
-		# update s
-		s_len = self.s.shape[0] if self.s.shape else 0
-		s = torch.FloatTensor(s_len + 1)
-		for i in xrange(s_len):
-			old = sum(self.s[j] for j in xrange(i + 1, s_len)) # TODO indexing right?
-			s[i] = self.relu(self.s[i] - self.relu(u - old))
-		s[s_len] = d
+		# update s, which is of size [t, batch_size]
+		old_t = self.s.shape[0] if self.s.shape else 0
+		s = torch.FloatTensor(old_t + 1, self.batch_size)
+		for i in xrange(old_t):
+			old = torch.sum(self.s[i + 1:old_t,:], 0) if i + 1 < old_t else torch.zeros(self.batch_size)
+			s[i,:] = relu(self.s[i,:] - relu(u - old))
+		s[old_t,:] = d
 		self.s = s
 
 		# calculate r
-		r = torch.zeros([1, v.shape[1]])
-		for i in xrange(s_len + 1):
-			old = sum(self.s[j] for j in xrange(i, s_len + 1)) # TODO indexing right?
-			# print "weight: {}, relu: {}".format(self.s[i], self.relu(old))
-			r += min(self.s[i], self.relu(old)) * self.V[i,:]
+		r = torch.zeros([self.batch_size, self.embedding_size])
+		for i in xrange(old_t + 1):
+			if i + 1 == old_t + 1:
+				continue
+			old = torch.sum(self.s[i + 1:old_t + 1,:], 0)
+			r += torch.min(self.s[i,:], relu(old)) * self.V[i,:,:]
 
 		return r
 
@@ -55,5 +60,18 @@ class Stack(nn.Module):
 		if not self.V.shape:
 			print "[Empty stack]"
 			return
-		for i in xrange(len(self.V)):
-			print "\t".join(str(x) for x in self.V[i,:]), "\t|\t", self.s[i]
+		for b in xrange(self.batch_size):
+			if b > 0:
+				print "----------------------------"
+			for i in xrange(self.V.shape[0]):
+				print "\t".join(str(x) for x in self.V[i, b,:]), "\t|\t", self.s[i, b]
+
+if __name__ == "__main__":
+	stack = Stack(2, 2)
+	out = stack.forward(torch.FloatTensor([[1, 2], [3, 4]]), torch.FloatTensor([1, 0]), torch.FloatTensor([0, 1]))
+	stack.log()
+	print out
+	print
+	out = stack.forward(torch.FloatTensor([[11, 22], [33, 44]]), torch.FloatTensor([1, 0]), torch.FloatTensor([1, 0]))
+	stack.log()
+	print out
