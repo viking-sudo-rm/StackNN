50c536a70c2997e4ef386b93e99f36df18907c99
Yiding Hao <yiding.hao@yale.edu>
2018-04-02 10:46:25 -0400

    Got rid of some redundant variables

--- reverse_experiment.py
+++ reverse_experiment.py
@@ -6,21 +6,17 @@ from tasks.reverse import ReverseTask
 
 m = __import__("model-bare")
 
-learning_rate = 0.1
-batch_size = 10
 read_size = 1
-epochs = 100
-
 task = ReverseTask(min_length=1,
                    mean_length=10,
                    std_length=2,
                    max_length=12,
-                   learning_rate=learning_rate,
-                   batch_size=batch_size,
+                   learning_rate=0.1,
+                   batch_size=10,
                    read_size=read_size,
                    cuda=False,
-                   epochs=epochs,
-                   model=m.FFController(3, 1, 3),
+                   epochs=100,
+                   model=m.FFController(3, read_size, 3),
                    criterion=nn.CrossEntropyLoss(),
                    verbose=True)
 
