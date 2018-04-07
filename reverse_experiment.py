d5894974a2a98d4665cbdb31972b8bfb196cb391
Yiding Hao <yiding.hao@yale.edu>
2018-04-06 20:09:10 -0400

    Added a learning rate of 0.01

--- reverse_experiment.py
+++ reverse_experiment.py
@@ -2,15 +2,16 @@ from __future__ import division
 
 import torch.nn as nn
 
-from tasks.reverse import ReverseTask
 from models.vanilla import Controller
+from tasks.reverse import ReverseTask
 
 read_size = 1
 task = ReverseTask(min_length=1,
                    mean_length=10,
                    std_length=2,
                    max_length=12,
-                   learning_rate=0.01,
+                   learning_rate=0.01,  # Must be 0.01
+                   l2_weight=0.01,
                    batch_size=10,
                    read_size=read_size,
                    cuda=False,
