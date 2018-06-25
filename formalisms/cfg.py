b09dd0acfb04a4499f8596ee2317c94398b85d9d
Yiding Hao <yiding@uchicago.edu>
2018-06-25 16:06:26 -0400

    Added Dana's xor grammars

--- formalisms/cfg.py
+++ formalisms/cfg.py
@@ -10,20 +10,17 @@ from __future__ import division
 
 from nltk import CFG
 
-
 dyck_grammar = CFG.fromstring("""
     S -> S S
     S -> '(' S ')' | '(' ')'
     S -> '[' S ']' | '[' ']'
 """)
 
-
 reverse_grammar = CFG.fromstring("""
     S -> "a" S "a1"
     S -> "b" S "b1"
     S -> "c"
 """)
-
 
 agreement_grammar = CFG.fromstring("""
     S -> NPsing "Auxsing"
@@ -45,4 +42,38 @@ agreement_grammar = CFG.fromstring("""
     Relobj -> "Rel" NPplur "Auxplur" "Vtrans"
     VP -> "Vintrans"
     VP -> "Vtrans" NP
+""")
+
+""" XOR Grammars """
+
+exp_eval_grammar = CFG.fromstring("""
+    S -> Strue | Sfalse
+    Strue -> "T" "1" 
+    Strue -> Strue Strue "and" "1"
+    Strue -> Strue Strue "or" "1" | Strue Sfalse "or" "1"
+    Strue -> Sfalse Strue "or" "1"
+    Sfalse -> "F" "0"
+    Sfalse -> Sfalse Sfalse "or" "0" 
+    Sfalse -> Sfalse Sfalse "and" "0" | Sfalse Strue "and" "0"
+    Sfalse -> Strue Sfalse "and" "0"
+""")
+
+xor_exp_eval_grammar = CFG.fromstring("""
+    S -> Strue | Sfalse
+    Strue -> "T" "1" 
+    Strue -> Strue Sfalse "xor" "1"
+    Strue -> Sfalse Strue "xor" "1"
+    Sfalse -> "F" "0"
+    Sfalse -> Sfalse Sfalse "xor" "0" 
+    Sfalse -> Strue Strue "xor" "0"
+""")
+
+xor_string_grammar = CFG.fromstring("""
+    S -> "0" "a" S | "1" "b" T | "0" "a" | "1" "b"
+    T -> "0" "b" T | "1" "a" S | "0" "b" | "1" "a"
+""")
+
+padded_xor_string_grammar = CFG.fromstring("""
+    S -> "0" "x" "a" S | "1" "x" "b" T | "0" "x" "a" | "1" "x" "b"
+    T -> "0" "x" "b" T | "1" "x" "a" S | "0" "x" "b" | "1" "x" "a"
 """)
