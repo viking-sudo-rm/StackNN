b5b94012c205a091b0098703faf013fef4528d1d
Yiding Hao <yiding@uchicago.edu>
2018-10-12 22:44:09 -0400

    Enabled autodoc
    Signed-off-by: Yiding Hao <yiding.hao@yale.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,10 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +34,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
