d7a20a3bc28d2ca6db0a26ac5083d0488cffb293
William Merrill <vikingarnir.will@gmail.com>
2018-11-26 11:22:58 -0500

    Switched test set to be the full one.

--- configs.py
+++ configs.py
@@ -270,7 +270,7 @@ anb2n_config = {
 linzen_agreement_config = {
     "task": NaturalTask,
     "train_path": "data/linzen/rnn_agr_simple/numpred.train",
-    "test_path": "data/linzen/rnn_agr_simple/numpred.test.5",
+    "test_path": "data/linzen/rnn_agr_simple/numpred.test",
     "data_reader": ByLineDatasetReader(linzen_line_consumer),
     "num_labels": 2,
     "batch_size": 16,
