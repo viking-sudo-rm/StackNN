e9e26a5f443c3bd736c78b0fd0ac49b8d6a3f94c
William Merrill <vikingarnir.will@gmail.com>
2018-11-26 00:24:41 -0500

    Added embedding layer to natural.py.

--- tasks/natural.py
+++ tasks/natural.py
@@ -43,18 +43,29 @@ class NaturalTask(Task):
             self.train_path = train_path
             self.test_path = test_path
             self.data_reader = data_reader
-            self.max_num_embeddings = kwargs.get("max_num_embeddings", 5000)
-            self.max_num_output_classes = kwargs.get("max_num_output_classes", 2)
+            self.embedding_dim = kwargs.get("embedding_dim", 300)
             super(NaturalTask.Params, self).__init__(**kwargs)
 
 
+    def __init__(self, params):
+        self.word_to_i = None
+        self.label_to_i = None
+        self.num_embeddings = None
+        self.num_labels = None
+
+        super(NaturalTask, self).__init__(params)
+
     @property
     def input_size(self):
-        return self.max_num_embeddings
+        return self.params.embedding_dim
 
     @property
     def output_size(self):
-        return self.max_num_output_classes
+        return len(self.label_to_i) + 1
+
+    @property
+    def num_embeddings(self):
+        return len(self.word_to_i) + 1
 
     def get_data(self):
         train_x, train_y = self.data_reader.read_x_and_y(self.train_path)
@@ -63,16 +74,19 @@ class NaturalTask(Task):
         pad_length = self._get_max_sentence_length(train_x, test_x)
 
         flatten = lambda li: (item for sublist in li for item in sublist)
-        vocab_x, word_to_i = self._get_vocab_and_lookup_table(flatten(train_x), flatten(test_x))
-        vocab_y, label_to_i = self._get_vocab_and_lookup_table(train_y, test_y)
+        vocab_x, self.word_to_i = self._get_vocab_and_lookup_table(flatten(train_x), flatten(test_x))
+        vocab_y, self.label_to_i = self._get_vocab_and_lookup_table(train_y, test_y)
 
         self.train_x = self._get_x_tensor(train_x, word_to_i, pad_length)
         self.train_y = self._get_y_tensor(train_y, label_to_i)
         self.test_x = self._get_x_tensor(test_x, word_to_i, pad_length)
         self.test_y = self._get_y_tensor(test_y, label_to_i)
 
-        print("train x shape:", self.train_x.shape)
-        print("train y shape:", self.train_y.shape)
+        self.embedding = nn.Embedding(self.num_embeddings, self.input_size)
+        print("Embedding layer initialized.")
+
+        print("Train x shape:", self.train_x.shape)
+        print("Train y shape:", self.train_y.shape)
 
     @staticmethod
     def _get_vocab_and_lookup_table(*datasets):
