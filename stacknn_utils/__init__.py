5fde6151136f1f8190348880fd19a2198e1e00b6
Will <william.merrill@yale.edu>
2019-03-03 18:22:32 -0500

    Updated struct implementation to use lists and terminate early.

--- stacknn_utils/__init__.py
+++ stacknn_utils/__init__.py
@@ -1,3 +1,5 @@
-from errors import *
-from validation import *
-from loggers import FileLogger, StringLogger
+from __future__ import absolute_import
+
+from .errors import *
+from .validation import *
+from .loggers import FileLogger, StringLogger
