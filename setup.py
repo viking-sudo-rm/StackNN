cf4b57171af5e2ef9081922ab105438201d42f03
Will Merrill <william.merrill@yale.edu>
2019-10-07 13:40:03 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name="stacknn",
+      version="0.1",
+      description="Differentiable stacks in PyTorch",
+      author="Computational Linguistics at Yale",
+      url="https://github.com/viking-sudo-rm/StackNN",
+      packages=["structs"],
+)
